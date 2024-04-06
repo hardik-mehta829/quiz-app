b31702e8270adcf51942d550430037700ecdb4fc
Hardik Mehta <imt_2021039@iiitm.ac.in>
2024-04-06 21:13:56 +0530

    Changing db

--- db.js
+++ db.js
@@ -4,7 +4,9 @@ dotenv.config({ path: './config.env' });
 
 const dbconnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(
+      'mongodb+srv://newuser:pH2pzX8G74i2V1P4@cluster0.jysaopw.mongodb.net/Quiz?retryWrites=true&w=majority'
+    );
     console.log('connected successfully');
   } catch (error) {
     console.log(error.message);
